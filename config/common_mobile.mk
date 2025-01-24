fd63f79ba9fac4eeb11f570185c7ca5fba3bba5c
DivestOS <tavi@divested.dev>
2025-01-24 15:55:50 +0100

    uncatched DOS scripted change(s)
    Change-Id: I6db30b41834b9666e5da2b3f8b30dfa062f1fe56

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -3,27 +3,22 @@ $(call inherit-product, vendor/lineage/config/common.mk)
 
 # Default notification/alarm sounds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.notification_sound=Pong.ogg \
+    ro.config.alarm_alert=Alarm_Buzzer.ogg
 
 # AOSP packages
 PRODUCT_PACKAGES += \
-    Email \
     ExactCalculator \
-    Exchange2
 
 # Lineage packages
 PRODUCT_PACKAGES += \
     Backgrounds \
-    Eleven \
     Etar \
     Jelly \
     Profiles \
     Seedvault
 
 ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
 endif
 
 ifeq ($(PRODUCT_TYPE), go)
